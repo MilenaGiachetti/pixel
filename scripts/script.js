bdc8beb05d4fe3874ca75002dffb83fb2c14fbfc
MilenaGiachetti <giachettimilena@gmail.com>
2020-01-08 01:12:05 -0300

    agregada funcion para borrar grilla

--- scripts/script.js
+++ scripts/script.js
@@ -2,14 +2,18 @@
 //creacion de grilla
 let container = document.getElementById('container');
 let gridMembers = new Array;
-for(let i = 0; i < 990; i++){
-    if(i%31 != 0){
-        let square = document.createElement('div');
-        (i%2 == 0)?square.classList.add('white'):square.classList.add('gray');
-        gridMembers.push(square)
-        container.appendChild(square);
+function makeGrid(){
+    for(let i = 0; i < 990; i++){
+        if(i%31 != 0){
+            let square = document.createElement('div');
+            (i%2 == 0)?square.classList.add('white'):square.classList.add('gray');
+            gridMembers.push(square)
+            container.appendChild(square);
+        }
     }
-}
+};
+makeGrid();
+
 //agregar eventListeners
 for(let i = 0 ; i< gridMembers.length; i++){
     gridMembers[i].addEventListener('mousedown', paint);
@@ -28,5 +32,13 @@ function addEvent(){
 function removeEvent(){
     for(let i = 0 ; i< gridMembers.length; i++){
         gridMembers[i].removeEventListener('mouseover', paint);
+    }
+}
+//clear
+let clearBtn = document.getElementById('clear');
+clearBtn.addEventListener('click', clear);
+function clear(){
+    for(let i = 0 ; i< gridMembers.length; i++){
+        gridMembers[i].classList.remove('red');
     }
 }
