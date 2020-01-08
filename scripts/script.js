6d4ef5f0dbd23161c1c2721737cebcfb2202a17b
MilenaGiachetti <giachettimilena@gmail.com>
2020-01-08 01:04:14 -0300

    funcionalidad basica de dibujo

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,32 @@
+
+//creacion de grilla
 let container = document.getElementById('container');
-for(let i = 0; i < 110; i++){
-    if(i%11 != 0){
+let gridMembers = new Array;
+for(let i = 0; i < 990; i++){
+    if(i%31 != 0){
         let square = document.createElement('div');
         (i%2 == 0)?square.classList.add('white'):square.classList.add('gray');
+        gridMembers.push(square)
         container.appendChild(square);
     }
-
 }
-
+//agregar eventListeners
+for(let i = 0 ; i< gridMembers.length; i++){
+    gridMembers[i].addEventListener('mousedown', paint);
+}
+function paint(){
+    this.classList.add('red');
+}
+//check if click is active
+document.addEventListener('mousedown', addEvent);
+document.addEventListener('mouseup', removeEvent);
+function addEvent(){
+    for(let i = 0 ; i< gridMembers.length; i++){
+        gridMembers[i].addEventListener('mouseover', paint);
+    }
+}
+function removeEvent(){
+    for(let i = 0 ; i< gridMembers.length; i++){
+        gridMembers[i].removeEventListener('mouseover', paint);
+    }
+}
