4c43f3c161c9433733c65d852f410e41b159274d
MilenaGiachetti <giachettimilena@gmail.com>
2020-01-08 00:36:29 -0300

    cambiado nombre de variable cuadro a ingles

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,10 @@
 let container = document.getElementById('container');
 for(let i = 0; i < 110; i++){
     if(i%11 != 0){
-        let cuadro = document.createElement('div');
-        (i%2 == 0)?cuadro.classList.add('white'):cuadro.classList.add('gray');
-        container.appendChild(cuadro);
+        let square = document.createElement('div');
+        (i%2 == 0)?square.classList.add('white'):square.classList.add('gray');
+        container.appendChild(square);
     }
 
 }
+
